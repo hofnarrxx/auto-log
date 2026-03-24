23d9c938ad2f5d4ae77f720b9a4f5b12bc4a2484
hofnarrxx <maksymparasiun@gmail.com>
2026-03-24 21:52:50 +0100

    change navigation bar

--- frontend/src/app/core/layout/app-layout/app-layout.ts
+++ frontend/src/app/core/layout/app-layout/app-layout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthApi } from '../../auth/auth-api';
 import { Router } from '@angular/router';
@@ -12,6 +12,21 @@ import { Router } from '@angular/router';
 export class AppLayout {
   private authApi = inject(AuthApi);
   private router = inject(Router);
+  menuOpen = signal(false);
+
+  toggleMenu(event: Event) {
+    event.stopPropagation();
+    this.menuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+
+  @HostListener('document:click')
+  onDocumentClick() {
+    this.menuOpen.set(false);
+  }
 
   logout() {
     this.authApi.logout().subscribe({
